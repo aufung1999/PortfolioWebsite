eca08890c631f923837c38d2517de245502a6753
aufung1999 <78595669+aufung1999@users.noreply.github.com>
2023-04-21 07:30:39 -0400

    udpate

--- components/portfolio/Portfolio.tsx
+++ components/portfolio/Portfolio.tsx
@@ -15,16 +15,18 @@ import ShowMany from "../utilities/ShowMany";
 import ShowOnce from "../utilities/ShowOnce";
 import Image from "next/image";
 
-type Props = {
-  src?: string | undefined;
-};
+// export interface ILogo {
+//   //   image: HTMLImageElement;
+//   src: string | StaticImageData;
+//   className?: string;
+// }
 
 const Portfolio = () => {
   const soloProjects = [
     {
       id: 1,
       title: "Sound Event Detection",
-      img: machineLearning,
+      img: machineLearning.src,
       description:
         "Integrating Machine Learning and Web Development, and it is like the Sound Event Detection of IPhone",
 
@@ -36,7 +38,7 @@ const Portfolio = () => {
     {
       id: 2,
       title: "Self-Learning Language",
-      img: selfLearningLang,
+      img: selfLearningLang.src,
       description:
         "Users can record words they are not familiar with, and memorize them through some mini-games",
       frontend: "React, Redux",
@@ -47,7 +49,7 @@ const Portfolio = () => {
     {
       id: 3,
       title: "Travel Calenther",
-      img: travelCalendar,
+      img: travelCalendar.src,
       description:
         "This website is for people who always travel around the world, so that they can know the local weather and temperature.",
       technologies: "JavaScript | Webpack",
@@ -119,13 +121,14 @@ const Portfolio = () => {
                 // style={{ animationDelay: `${index}s` }}
               >
                 <div className={styles["portfolio__item-image"]}>
-                  {/* <img src={pro.img} alt={pro.title} /> */}
-                  <Image
+                  <img src={pro.img} alt={pro.title} />
+                  {/* <Image
+                    className="object-cover"
                     src={pro.img}
                     alt={pro.title}
                     width={200}
                     height={200}
-                  />
+                  /> */}
                 </div>
                 <div className={styles["portfolio__item-content"]}>
                   <h3>
