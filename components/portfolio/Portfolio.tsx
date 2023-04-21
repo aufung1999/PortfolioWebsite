633391031d30bb332c33a0bdc83bbeeafc0b6171
aufung1999 <78595669+aufung1999@users.noreply.github.com>
2023-04-21 07:37:49 -0400

    update

--- components/portfolio/Portfolio.tsx
+++ components/portfolio/Portfolio.tsx
@@ -6,9 +6,9 @@ import IMG4 from "../../public/assets/fake.png";
 import IMG5 from "../../public/assets/news.png";
 import IMG6 from "../../public/assets/math.png";
 
-import machineLearning from "../../public/assets/machineLearning.png";
-import selfLearningLang from "../../public/assets/selfLearningLang.png";
-import travelCalendar from "../../public/assets/travelCalendar.png";
+import ML from "../../public/assets/machineLearning.png";
+import SL from "../../public/assets/selfLearningLang.png";
+import TC from "../../public/assets/travelCalendar.png";
 
 import styles from "./Portfolio.module.css";
 import ShowMany from "../utilities/ShowMany";
@@ -26,7 +26,7 @@ const Portfolio = () => {
     {
       id: 1,
       title: "Sound Event Detection",
-      img: machineLearning.src,
+      img: ML.src,
       description:
         "Integrating Machine Learning and Web Development, and it is like the Sound Event Detection of IPhone",
 
@@ -38,7 +38,7 @@ const Portfolio = () => {
     {
       id: 2,
       title: "Self-Learning Language",
-      img: selfLearningLang.src,
+      img: SL.src,
       description:
         "Users can record words they are not familiar with, and memorize them through some mini-games",
       frontend: "React, Redux",
@@ -49,7 +49,7 @@ const Portfolio = () => {
     {
       id: 3,
       title: "Travel Calenther",
-      img: travelCalendar.src,
+      img: TC.src,
       description:
         "This website is for people who always travel around the world, so that they can know the local weather and temperature.",
       technologies: "JavaScript | Webpack",
